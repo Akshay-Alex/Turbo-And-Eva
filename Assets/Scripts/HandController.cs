32058fb77d615ecf61f4d4a29b8ff619946879a1
Akshay Alex <akshay.alex5@gmail.com>
2024-10-07 11:32:51 +0530

    IK still needs work.

--- Assets/Scripts/HandController.cs
+++ Assets/Scripts/HandController.cs
@@ -1,34 +1,165 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class HandController : MonoBehaviour
 {
+    public class PositionAndTarget
+    {
+        public Vector3 position;
+        public GameObject target;
+
+        public PositionAndTarget(Vector3 pos, GameObject gameObject)
+        {
+            position = pos;
+            target = gameObject;
+        }
+    }
     public Camera mainCamera;
     public GameObject[] HandTargets;// Assign the main camera (optional, if using Camera.main)
     public Vector3 InputPositionVector;
+    //List<GameObject> targetsBeingControlled, freeTargets;
+    Dictionary<Touch, GameObject> touchAndTargets = new Dictionary<Touch, GameObject>();
     void Update()
     {
         if (Input.touchCount > 0) // 0 is for left mouse button
         {
+
             for (int i = 0; i < Input.touchCount; i++)
             {
                 Touch touch = Input.GetTouch(i);
 
                 // Only process if the touch has just begun (TouchPhase.Began)
-                if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved)
+                if (touch.phase == TouchPhase.Began)
                 {
-                    HandleTouch(touch);
+                    Debug.Log("Added touch "+touch.fingerId);
+                    OnNewTouch(touch);
+                }
+                if (touch.phase == TouchPhase.Moved)
+                {
+                    //OnMovedExistingTouch(touch);
+                }
+                if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
+                {
+                    Debug.Log("Remove touch " + touch.fingerId);
+                    touchAndTargets.Remove(touch);
+                    /*
+                    if (touchAndTargets.ContainsKey(touch))
+                    {
+                        touchAndTargets.Remove(touch);
+                    }
+                    */
                 }
             }
             
         }
     }
-
-
-    void MoveHand(Vector3 targetPosition,int Index)
+    private void Start()
     {
-        InputPositionVector = new Vector3(targetPosition.x, targetPosition.y, 0);
-        HandTargets[Index].transform.localPosition = InputPositionVector;
+     
     }
+
+    void MoveHand(Vector3 targetPosition,GameObject target)
+    {
+        Debug.Log("Move target "+target+" to position "+targetPosition);
+        InputPositionVector = new Vector3(targetPosition.x, targetPosition.y, 0);
+        target.transform.localPosition = InputPositionVector;
+    }
+    public PositionAndTarget FindNearestFreeTarget(Touch touch)
+    {
+        Ray ray = mainCamera.ScreenPointToRay(touch.position);
+        RaycastHit hit;
+        // Check if the ray hits any object with a collider
+        if (Physics.Raycast(ray, out hit))
+        {
+            // Check if the object hit has a specific tag (e.g., "Plane") or any object you want
+            if (hit.collider.CompareTag("Plane"))
+            {
+                float shortestDistance = 1000;
+                int index = -1,nearestTargetIndex = -1;
+                foreach (GameObject target in HandTargets)
+                {
+                    index++;
+                    if (!touchAndTargets.ContainsValue(target))
+                    {
+                        float currentDistance = Vector3.Distance(target.transform.position, hit.point);
+                        if (currentDistance < shortestDistance)
+                        {
+                            shortestDistance = currentDistance;
+                            nearestTargetIndex = index;
+                        }
+                        
+                    }
+
+                }
+                if(nearestTargetIndex > -1)
+                {
+                    return new PositionAndTarget(hit.point, HandTargets[nearestTargetIndex]);
+                }
+            }
+        }
+        return new PositionAndTarget(Vector3.one, null);
+    }
+    public PositionAndTarget FindNearestTarget(Touch touch)
+    {
+        Ray ray = mainCamera.ScreenPointToRay(touch.position);
+        RaycastHit hit;
+        // Check if the ray hits any object with a collider
+        if (Physics.Raycast(ray, out hit))
+        {
+            // Check if the object hit has a specific tag (e.g., "Plane") or any object you want
+            if (hit.collider.CompareTag("Plane"))
+            {
+                float shortestDistance = Vector3.Distance(HandTargets[0].transform.position, hit.point);
+                int index = -1, nearestTargetIndex = -1;
+                foreach (GameObject target in HandTargets)
+                {
+                    index++;
+                    if (touchAndTargets.ContainsValue(target))
+                    {
+                        float currentDistance = Vector3.Distance(target.transform.position, hit.point);
+                        if (currentDistance < shortestDistance)
+                        {
+                            shortestDistance = currentDistance;
+                            nearestTargetIndex = index;
+                        }
+
+                    }
+
+                }
+                if (nearestTargetIndex > -1)
+                {
+                    return new PositionAndTarget(hit.point, HandTargets[nearestTargetIndex]);
+                }
+            }
+        }
+        return new PositionAndTarget(Vector3.one, null);
+    }
+    void AttachTouchToTarget(Vector3 touchPosition, int TargetIndex)
+    {
+
+    }
+    void OnNewTouch(Touch touch)
+    {
+        PositionAndTarget positionAndTarget = FindNearestFreeTarget(touch);
+        Debug.Log("searching for free target");
+        if (positionAndTarget.target)
+        {
+            Debug.Log("Free target found");
+            touchAndTargets.Add(touch, positionAndTarget.target);
+            MoveHand(positionAndTarget.position, positionAndTarget.target);
+        }
+    }
+    void OnMovedExistingTouch(Touch touch)
+    {
+        PositionAndTarget positionAndTarget = FindNearestTarget(touch);
+        if (positionAndTarget.target)
+        {
+            //touchAndTargets.Add(touch, positionAndTarget.target);
+            MoveHand(positionAndTarget.position, positionAndTarget.target);
+        }
+    }
+    /*
     void HandleTouch(Touch touch)
     {
         // Convert the touch position to a ray
@@ -46,17 +177,23 @@ public class HandController : MonoBehaviour
                 int index = 0;
                 foreach (GameObject target in HandTargets)
                 {
-                    float currentDistance = Vector3.Distance(target.transform.position, hit.point);
-                    if(currentDistance < shortestDistance)
+                    if(!touchAndTargets.ContainsValue(target))
                     {
-                        shortestDistance = currentDistance;
-                        nearestTargetIndex = index;
+                        float currentDistance = Vector3.Distance(target.transform.position, hit.point);
+                        if (currentDistance < shortestDistance)
+                        {
+                            shortestDistance = currentDistance;
+                            nearestTargetIndex = index;
+                        }
+                        index++;
+                        touchAndTargets.Add(touch, HandTargets[nearestTargetIndex]);
+                        MoveHand(hit.point, nearestTargetIndex);
                     }
-                    index++;
-                    MoveHand(hit.point,nearestTargetIndex);
+                    
                 }
                 //MoveHand(hit.point,touch.fingerId);
             }
         }
     }
+    */
 }
